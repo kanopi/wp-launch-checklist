2114ace17cfd57fbe4e81246b45e324c4d330dee
Damon Sharp <damon.sharp@gmail.com>
2021-08-27 16:00:57 -0400

    stub congifuration file

---
+++ includes/checklist_items_config.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Checklist items organized by group, then checklist items.
+ */
+return [
+	[
+		'group'       => __( 'Group 1', 'kanopi' ),
+		'description' => __( 'Description here. Proin lacus erat tristique integer suspendisse ullamcorper ultricies orci mi aenean lacinia fames', 'kanopi' ),
+		'items'       => [
+			[
+				'description' => '',
+				'name'        => '',
+				'label'       => '',
+			],
+			[
+				'description' => '',
+				'name'        => '',
+				'label'       => '',
+			],
+			[
+				'description' => '',
+				'name'        => '',
+				'label'       => '',
+			],
+			[
+				'description' => '',
+				'name'        => '',
+				'label'       => '',
+			],
+		],
+	],
+	[
+		'group'       => __( 'Group 2', 'kanopi' ),
+		'description' => __( 'Fermentum dapibus eget ac rhoncus feugiat quisque torquent porta elit elementum class.',
+		                     'kanopi' ),
+		'items'       => [
+			[
+				'description' => '',
+				'name'        => '',
+				'label'       => '',
+			],
+			[
+				'description' => '',
+				'name'        => '',
+				'label'       => '',
+			],
+			[
+				'description' => '',
+				'name'        => '',
+				'label'       => '',
+			],
+			[
+				'description' => '',
+				'name'        => '',
+				'label'       => '',
+			],
+		],
+	],
+];
